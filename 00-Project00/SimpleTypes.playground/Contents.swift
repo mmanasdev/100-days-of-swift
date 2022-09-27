75b7c8ab6919faa66db1d057ffa4bab575f9de2a
Miguel M <miguel.manas.alvarez.dev@gmail.com>
2022-09-27 07:25:08 +0200

    Simple Types
    Variables
    Optional: Why does Swift have variables?
    Test: Variables
    Strings and integers
    Optional: Why is Swift a type-safe language?
    Test: Strings and integers
    Multi-line strings
    Optional: Why does Swift need multi-line strings?
    Test: Multi-line strings
    Doubles and Booleans
    Optional: Why does Swift need both Doubles and Integers?
    Test: Doubles and Booleans
    String interpolation
    Optional: Why does Swift have string interpolation?
    Test: String interpolation
    Constants
    Optional: Why does Swift have constants as well as variables?
    Test: Constants
    Type annotations
    Optional: Why does Swift have type annotations?
    Test: Type annotations
    Simple types: Summary
    Test: Simple types

---
+++ 00-Project00/SimpleTypes.playground/Contents.swift
@@ -0,0 +1,88 @@
+import UIKit
+
+//MARK:- DAY 1:
+
+//	Variables
+
+var str = "Hello, playground"
+str = "Goodbye"
+
+//Why does Swift have variables?
+
+var favoriteShow = "Orange is the new black"
+favoriteShow = "The Good Place"
+favoriteShow = "Doctor Who"
+
+//	Strings and integers
+
+var age = 38
+var population = 8_000_000
+
+//Why is Swift a type-safe language?
+
+var meaningOfLife = 42
+
+
+//	Multi-line strings
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+//Why does Swift need multi-line strings?
+var quote = "Change the worldby being yourself"
+var burns = """
+The best laid schemes
+O' mice and men
+Gang aft agley
+"""
+
+
+//	Doubles and booleans
+
+var pi = 3.141
+var awesoem = true
+
+//Why does Swift need both Doubles and Integers?
+var myInt = 1
+var myDouble = 1.0
+
+
+//	String interpolation
+
+var score = 85
+var result1 = "Your score was \(score)"
+var result2 = "The test results are here: \(result1)"
+
+//Why does Swift have string interpolation?
+
+var city = "Cardiff"
+var message = "Welcome to \(city)!"
+
+
+//	Constants
+
+let taylor = "swift"
+
+//Why does Swift have constants as well as variables?
+
+
+//	Type annotations
+
+let greatings = "Hello, Playground"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+//Why does Swift have type annotations?
+var percentage: Double = 99
+
